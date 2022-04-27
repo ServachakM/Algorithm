b5ddc4cb5f0b0df07cc0098ce7676c9e2163b2e2
ServachakM <servachak.m.u@gmail.com>
2022-04-28 01:44:06 +0300

    Code cleaned

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -3,7 +3,7 @@ public class AscendingSequence {
     // 0,1,2,3,4,5
     public int[] ascendingSequenceAlgorithm(int start, int end, int step){
         if(step > 0 && start < end){
-           int[] result = new int[(end - start + 1) / step];
+           int[] result = new int[((end - start) / step) + 1];
 
 
             for (int i = 0; i < result.length ; i ++) {
