952adfdd72e1a122b7ee9bfab249d590c171b700
ServachakM <servachak.m.u@gmail.com>
2022-04-30 18:49:20 +0300

    Algorithm ReverseArray + test

---
+++ src/main/java/ReverseArray.java
@@ -0,0 +1,18 @@
+public class ReverseArray {
+
+    public int[] reverseArray(int[] array){
+        int[] result = {};
+        if(array.length > 0){
+            result = new int[array.length];
+            int count = 0;
+            for (int i = array.length - 1; i > -1; i--) {
+               result[count] = array[i];
+               count++;
+            }
+            return result;
+        }
+        return result;
+    }
+
+
+}
