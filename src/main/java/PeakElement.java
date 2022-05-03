4e3d846a1485420b648d69638c1725ce1a803acc
ServachakM <servachak.m.u@gmail.com>
2022-05-04 01:09:12 +0300

    Code cleaned

--- src/main/java/PeakElement.java
+++ src/main/java/PeakElement.java
@@ -1,129 +1,131 @@
 public class PeakElement {
 
-  public static int[] peakElement(int[] array) {
+    public static int[] peakElement(int[] array) {
 
-    int[] result = {};
-    int count = 0;
+        int[] result = {};
+        int count = 0;
 
-    if (array.length > 0) {
-      if (array[0] > array[1] || array[array.length - 1]
-              > array[array.length - 2]) {
-        count++;
-      }
-      for (int i = 1; i < array.length - 1; i++) {
-        if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
-          count++;
+        if (array.length > 0) {
+            if (array[0] > array[1] || array[array.length - 1]
+                    > array[array.length - 2]) {
+                count++;
+            }
+            for (int i = 1; i < array.length - 1; i++) {
+                if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+
+                return result;
+            }
+            result = new int[count];
+            count = 0;
+            for (int i = 0; i < array.length; ) {
+                if (i == 0) {
+                    if (array[i] > array[i + 1]) {
+                        result[count] = array[i];
+                        count++;
+                        i++;
+                    } else {
+                        i++;
+                    }
+                } else if (i < array.length - 1 && array[i] > array[i - 1] && array[i] > array[i + 1]) {
+                    result[count] = array[i];
+                    count++;
+                    i++;
+                } else if (i == array.length - 1) {
+                    if (array[array.length - 1] > array[array.length - 2] && count < result.length) {
+                        result[count] = array[array.length - 1];
+                    }
+                }
+            }
+
+            return result;
         }
-      }
-      if (count == 0) {
 
         return result;
-      }
-      result = new int[count];
-      count = 0;
-      for (int i = 0; i < array.length; ) {
-        if (i == 0) {
-          if (array[i] > array[i + 1]) {
-            result[count] = array[i];
-            count++;
-            i ++;
-          } else {
-            i ++;
-          }
-        } else if (i < array.length - 1 && array[i] > array[i - 1] && array[i] > array[i + 1]) {
-          result[count] = array[i];
-          count++;
-          i ++;
-        } else if (i == array.length - 1) {
-          if (array[array.length - 1] > array[array.length - 2] && count < result.length) {
-            result[count] = array[array.length - 1];
-          }
-        }
-      }
-
-      return result;
     }
 
-    return result;
-  }
+    public int[] peakElement2(int[] arr) {
+        int[] arr2 = new int[arr.length];
+        int counter = 0;
+        if (arr.length > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                if (i == 0) {
+                    if (arr[i] > arr[i + 1]) {
+                        arr2[counter] = arr[i];
+                        counter++;
+                    }
+                } else if (i == arr.length - 1) {
+                    if (arr[i] > arr[i - 1]) {
+                        arr2[counter] = arr[i];
+                        counter++;
+                    }
+                } else if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
+                    arr2[counter] = arr[i];
+                    counter++;
+                }
+            }
+            int[] result = new int[counter];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = arr2[i];
+            }
 
-  public int[] peakElement2(int[] arr) {
-    int[] arr2 = new int[arr.length];
-    int counter = 0;
-    for (int i = 0; i < arr.length; i++) {
-      if (i == 0) {
-        if (arr[i] > arr[i + 1]) {
-          arr2[counter] = arr[i];
-          counter++;
+            return result;
         }
-      } else if (i == arr.length - 1) {
-        if (arr[i] > arr[i - 1]) {
-          arr2[counter] = arr[i];
-          counter++;
-        }
-      } else if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
-          arr2[counter] = arr[i];
-          counter++;
-        }
-    }
-    int[] result = new int[counter];
-    for (int i = 0; i < result.length; i++) {
-      result[i] = arr2[i];
-    }
 
-    return result;
-  }
-
-
-  public static int getMaxValue(int index1, int index2){
-    return (index1 > index2) ? index1 : index2;
-  }
-
-  public static int getMaxValueNew(int index1, int index2){
-    return (index1 > index2) ? index1 : 0;
-  }
-
-  public static int[] peakInNeighbourElement(int array[]){
-    int [] peakArray = new int[array.length];
-
-    for (int i = 0; i < array.length; i++) {
-
-      if(i == 0){
-        if(getMaxValueNew(array[i], array[i + 1]) > 0){
-          peakArray[i] = getMaxValueNew(array[i], array[i + 1]);
-        }
-      }
-      else if(i > 0 && i < array.length - 1 ){
-        if(getMaxValueNew(array[i], array[i -1]) > 0){
-          peakArray[i] = getMaxValueNew(getMaxValue(array[i - 1], array[i]),array[i + 1] );
-        }
-      }
-      else if(i == array.length - 1){
-        peakArray[i] = getMaxValueNew(array[i], array[i - 1]);
-      }
-    }
-    return peakArray;
-  }
-
-  public static int[] getElements(int array[]){
-
-    int counter = 0;
-    for (int i = 0; i < array.length; i++) {
-      if(array[i] != 0){
-        counter++;
-      }
+        return new int[]{};
     }
 
-    int elements [] = new int[counter];
-    counter = 0;
-    for (int i = 0; i < array.length; i++) {
-      if(array[i] != 0){
-        elements[counter] = array[i];
-        counter++;
-      }
+
+    public static int getMaxValue(int index1, int index2) {
+        return (index1 > index2) ? index1 : index2;
     }
-    return elements;
-  }
+
+    public static int getMaxValueNew(int index1, int index2) {
+        return (index1 > index2) ? index1 : 0;
+    }
+
+    public int[] peakInNeighbourElement(int array[]) {
+        int[] peakArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (i == 0) {
+                if (getMaxValueNew(array[i], array[i + 1]) > 0) {
+                    peakArray[i] = getMaxValueNew(array[i], array[i + 1]);
+                }
+            } else if (i > 0 && i < array.length - 1) {
+                if (getMaxValueNew(array[i], array[i - 1]) > 0) {
+                    peakArray[i] = getMaxValueNew(getMaxValue(array[i - 1], array[i]), array[i + 1]);
+                }
+            } else if (i == array.length - 1) {
+                peakArray[i] = getMaxValueNew(array[i], array[i - 1]);
+            }
+        }
+        return peakArray;
+    }
+
+    public int[] getElements(int array[]) {
+
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                counter++;
+            }
+        }
+
+        int elements[] = new int[counter];
+        counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                elements[counter] = array[i];
+                counter++;
+            }
+        }
+        return elements;
+    }
 
 }
 
--- src/test/java/PeakElementTest.java
+++ src/test/java/PeakElementTest.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class PeakElementTest {
+import org.junit.jupiter.api.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class PeakElementTest {
+    PeakElement peak;
+
+    @BeforeEach
+    void setup (){
+        peak = new PeakElement();
+    }
+
+    @Order(1)
+    @Test
+    public void testPeakElementHappyPath(){
+        int[] array = {3, 2, 7, 5, 1, 9, 23, 1};
+
+        int[] expectedResult = {3, 7, 23};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(2)
+    @Test
+    public void testPeakElementHappyPathNegativeParam(){
+        int[] array = {-3, 2, 7, -5, 1, 9, -23, 1};
+
+        int[] expectedResult = {7, 9, 1};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(3)
+    @Test
+    public void testPeakElementParamWithZero(){
+        int[] array = {0, -2, -7, -5, 0, -1};
+
+        int[] expectedResult = {0, 0};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(4)
+    @Test
+    public void testPeakElementArrayEmpty(){
+        int[] array = {};
+
+        int[] expectedResult = {};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(5)
+    @Test
+    public void testPeakElementEqual(){
+        int[] array = {8, 8, 8, 8, 8};
+
+        int[] expectedResult = {};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(6)
+    @Test
+    public void testPeakElementMaxValue(){
+        int[] array = {Integer.MAX_VALUE, 25, 31, Integer.MAX_VALUE};
+
+        int[] expectedResult = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+
+        int[] actualResult = peak.peakElement2(array);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
 }
