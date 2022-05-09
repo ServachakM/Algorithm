0e2947cd71c26f4a60914d77fb49bdacea10d62f
ServachakM <servachak.m.u@gmail.com>
2022-05-09 21:52:26 +0300

    Algorithm String to numbers + test

---
+++ src/main/java/StringToNumbers.java
@@ -0,0 +1,33 @@
+public class StringToNumbers {
+
+    public int[] stringToNumbers(String str) {
+        if (str != null) {
+            str = str.trim();
+            if (str.length() > 0) {
+                int count = 0;
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) > 47 && str.charAt(i) < 58) {
+                        count++;
+                    }
+
+                }
+
+                int[] result = new int[count];
+                count = 0;
+
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) > 47 && str.charAt(i) < 58) {
+                        result[count] = Integer.parseInt(Character.toString(str.charAt(i)));
+                        count++;
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        return new int[]{};
+    }
+
+
+}
---
+++ src/test/java/StringToNumbersTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class StringToNumbersTest {
+     StringToNumbers result;
+
+     @BeforeEach
+    void setup(){
+         result = new StringToNumbers();
+     }
+
+     @Order(1)
+     @Test
+    public void testStringToNumbersHappyPath(){
+         String str = "1, 2, 3, 4, 5";
+
+         int[] expectedResult = {1, 2, 3, 4, 5};
+
+         int[] actualResult = result.stringToNumbers(str);
+
+         Assertions.assertArrayEquals(expectedResult, actualResult);
+     }
+
+     @Order(2)
+     @Test
+    public void testStringToNumbersHappyPathNumbersPlusLetters(){
+        String str = "a1, b2, c3, d4, e5";
+
+        int[] expectedResult = {1, 2, 3, 4, 5};
+
+        int[] actualResult = result.stringToNumbers(str);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(3)
+    @Test
+    public void testStringToNumbersMoreSpace(){
+        String str = "   a1,     b2, c3, d4, e5    ";
+
+        int[] expectedResult = {1, 2, 3, 4, 5};
+
+        int[] actualResult = result.stringToNumbers(str);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(4)
+    @Test
+    public void testStringToNumbersHappyPathAnotherNumbers(){
+        String str = "0, 9, 2, 7, 0";
+
+        int[] expectedResult = {0, 9, 2, 7, 0};
+
+        int[] actualResult = result.stringToNumbers(str);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Order(5)
+    @Test
+    public void testStringToNumbersDifferentSymbol(){
+        String str = "-SSS--kju0,bvnm'./,9,,,,2vcgf,mklk7, +0  ";
+
+        int[] expectedResult = {0, 9, 2, 7, 0};
+
+        int[] actualResult = result.stringToNumbers(str);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+}
